6e547b5ed487a4fa39c4aabb9cc9d7ab45d0ab4d
Laura Gabriely <laura.587523la@gmail.com>
2025-05-02 15:50:53 -0300

    feat: adiciona endpoint "GET /api/health"

--- app/api/router.py
+++ app/api/router.py
@@ -1,5 +1,25 @@
 from fastapi import APIRouter
+from pydantic import BaseModel, Field
 
 routes = APIRouter()
 
 # XXX Adcionar rotas....
+
+VERSAO_API ="0.0.1"
+
+class RootResponse(BaseModel):
+    """
+    Modelo de resposta para o endpoint raiz.
+    """
+    versao: str = Field(
+        VERSAO_API,
+        description="Versão da API"
+        )
+
+@routes.get("/")
+async def get_root():
+    return {"message": "Bem vindo ao PC-Estoque"}
+
+@routes.get("/api/health")
+async def get_version()-> RootResponse:
+    return RootResponse(versao=VERSAO_API)
