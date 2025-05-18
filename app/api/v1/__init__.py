c2074ca440410f176123bece7aff0b30414fbcc7
Laura Gabriely <laura.587523la@gmail.com>
2025-05-18 03:40:40 -0300

    refactor: alteração no nome da tag da rota "/api"

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from app.settings import api_settings
 
 
-router_estoque = APIRouter(prefix="/api", tags=["estoque"])
+router_estoque = APIRouter(prefix="/api", tags=["API"])
 
 
 def load_routes(router_estoque: APIRouter):
