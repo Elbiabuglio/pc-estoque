61ddb5735ce4341a3a1857e183d0c12ae4cd2a93
Laura Gabriely <laura.587523la@gmail.com>
2025-06-03 00:44:51 -0300

    refactor: remove importação desnecessária do roteador Estoque

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -10,7 +10,6 @@ router_estoque_v2 = APIRouter(prefix="/seller/v2", tags=["Estoque V2"])
 
 def load_routes(router_estoque: APIRouter):
     if api_settings.enable_estoque_resources:
-        from app.api.v2.routers.estoque_router import router
 
         router_estoque.include_router(estoque_router_v2)
 
